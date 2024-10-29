74e61f12c61ce06493f2b82bb7e8bcac107101e3
Trantor2098 <trantor2021@163.com>
2024-10-30 00:16:14 +0800

    0.7.3 update

--- constants.py
+++
@@ -1,5 +0,0 @@
-node_group_id_names = {"ShaderNodeGroup", "GeometryNodeGroup", "CompositorNodeGroup", "TextureNodeGroup"}
-tree_type_id_names = {"ShaderNodeTree", "GeometryNodeTree", "CompositorNodeTree", "TextureNodeTree"}
-
-# dev 1, user None
-preset_json_indent = None
